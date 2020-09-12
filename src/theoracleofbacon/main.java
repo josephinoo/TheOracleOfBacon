0af807897e456780fc1f8bd30e3a938dda4a247c
Joseph <josdavil@espol.edu.ec>
2020-09-12 15:16:15 -0500

    main fxml

--- src/theoracleofbacon/main.java
+++ src/theoracleofbacon/main.java
@@ -1,18 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package theoracleofbacon;
 
 
+import java.io.IOException;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
+/**
+ *
+ * @author soyjosephavila
+ */
 public class main extends Application {
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void start(Stage primaryStage) throws IOException {
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/main.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene (root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
-
+        
+    public static void main(String[] args) {
+        Application.launch(args);  
+    }
 }
