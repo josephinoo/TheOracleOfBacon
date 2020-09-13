96586e08d686c798cf326755319f68f8bcb09993
JaimePizarro <japibaro@espol.edu.ec>
2020-09-13 12:52:03 -0500

    VBox FXML

--- src/view/MainController.java
+++ src/view/MainController.java
@@ -28,6 +28,8 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import theoracleofbacon.NumberBacon;
 import util.Vertex;
@@ -43,6 +45,8 @@ public class MainController implements Initializable {
     private TextField person1;
     @FXML
     private TextField person2;
+    @FXML
+    private VBox vbDijkstra;
 
     private ContextMenu entriesPopup;
     private final SortedSet<String> entries;
@@ -55,10 +59,9 @@ public class MainController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         entries.addAll(actores());
-        System.out.println(actores());
         autocompleteTextField(person1);
         autocompleteTextField(person2);
-
+        addEdges(3,vbDijkstra);
     }
 
     private void autocompleteTextField(TextField person) {
@@ -74,7 +77,7 @@ public class MainController implements Initializable {
                     LinkedList<String> searchResult = new LinkedList<>();
                     searchResult.addAll(entries.subSet(person.getText(), person.getText() + Character.MAX_VALUE));
                     if (entries.size() > 0) {
-                        populatePopup(searchResult,person);
+                        populatePopup(searchResult, person);
                         if (!entriesPopup.isShowing()) {
                             entriesPopup.show(person, Side.BOTTOM, 0, 0);
                         }
@@ -87,7 +90,7 @@ public class MainController implements Initializable {
         );
     }
 
-    private void populatePopup(List<String> searchResult,TextField person) {
+    private void populatePopup(List<String> searchResult, TextField person) {
         List<CustomMenuItem> menuItems = new LinkedList<>();
         // If you'd like more entries, modify this line.
         int maxEntries = 10;
@@ -117,34 +120,38 @@ public class MainController implements Initializable {
         }
         return actores;
     }
-    public void addEdges(int shape ,VBox vBoxPane ){
-      for(int i =0 ;i<shape;i++){
-        StackPane stackRectangle= new StackPane();
-        Rectangle rectangle = new Rectangle(200, 50);
-         StackPane stackLine= new StackPane();
-        Line line= new Line(100,0,100,30);
-        if(i%2==0){
-        rectangle.setFill(Color.rgb(189,253,178));
-        Text texto= new Text("was");
-        stackLine.getChildren().add(texto);
-        }
-        else {
-            rectangle.setFill(Color.rgb(172,172,253));
-            Text texto= new Text("with in ");
-             stackLine.getChildren().add(texto);
-        }
-        rectangle.setStroke(Color.BLACK);
-        rectangle.setStrokeWidth(0.5);
-        
-        stackRectangle.getChildren().addAll(rectangle);
-        vBoxPane.getChildren().addAll(stackRectangle);
-       
-        stackLine.getChildren().addAll(line);
-        if(i<shape-1){
-        vBoxPane.getChildren().addAll(stackLine);}
 
-      
-      
-      }
-}
+    public void addEdges(int shape, VBox vBoxPane) {
+        for (int i = 0; i < shape; i++) {
+            StackPane stackRectangle = new StackPane();
+            Rectangle rectangle = new Rectangle(200, 50);
+            StackPane stackLine = new StackPane();
+            Line line = new Line(100, 0, 100, 30);
+            line.setStroke(Color.GRAY);
+            line.setStrokeWidth(3);
+            stackLine.getChildren().addAll(line);
+            if (i % 2 == 0) {
+                rectangle.setFill(Color.rgb(189, 253, 178));
+                Text texto = new Text("was in");
+                texto.setFont(Font.font("Arial",FontWeight.BOLD,14));
+                stackLine.getChildren().add(texto);
+            } else {
+                rectangle.setFill(Color.rgb(172, 172, 253));
+                Text texto = new Text("with");
+                texto.setFont(Font.font("Arial",FontWeight.BOLD,14));
+
+                stackLine.getChildren().add(texto);
+            }
+            rectangle.setStroke(Color.BLACK);
+            rectangle.setStrokeWidth(0.5);
+
+            stackRectangle.getChildren().addAll(rectangle);
+            vBoxPane.getChildren().addAll(stackRectangle);
+
+            if (i < shape - 1) {
+                vBoxPane.getChildren().addAll(stackLine);
+            }
+
+        }
+    }
 }
